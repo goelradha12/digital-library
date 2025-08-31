6ac808ec5eb2828a6cc3f53ebf4326cd4beb132a
Radha <goyalradha2001@gmail.com>
2025-08-31 12:28:34 +0530

    Added about us page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import BookPage from './pages/BookPage';
+import AboutUs from './pages/AboutUs';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/book" element={<BookPage />} />
+      <Route path="/aboutUs" element={<AboutUs />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </BrowserRouter>
