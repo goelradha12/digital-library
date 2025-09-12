dbe7057ce030dfcc2874e7b98d68d390d0bf80c5
a-kriti123 <akritisharma2344@gmail.com>
2025-09-13 00:01:49 +0530

    style folder

---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            animation: {
+                'gradientMove': 'gradientMove 8s linear infinite',
+            },
+            keyframes: {
+                gradientMove: {
+                    '0%, 100%': {
+                        'background-position': '0% 50%',
+                    },
+                    '50%': {
+                        'background-position': '100% 50%',
+                    },
+                },
+            },
+        },
+    },
+    plugins: [],
+}
