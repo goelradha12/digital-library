f8b6676460f2e23dd8f8e1b1e1488246eddd6f67
Radha <goyalradha2001@gmail.com>
2025-09-15 12:30:45 +0530

    Basic server added

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
+import bookRouter from "./routers/Book.Routers.js";
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("App listening at port: " + port);
+});
+
+app.get("/", async (req, res) => {
+  // Do something with the connection
+  res.send("I am here");
+});
+
+app.use("/books", bookRouter);
