d767d3cbbcb202b349d31ee62c22e442858c9d58
Radha <goyalradha2001@gmail.com>
2026-07-05 19:44:35 +0530

    Correcting again

--- database/setup.sql
+++ database/setup.sql
@@ -213,6 +213,7 @@ DELIMITER ;
 
 -- Publisher_ID
 DELIMITER $$
+
 CREATE TRIGGER  generate_Publisher_ID 
 before insert on Publisher
 For each ROW
@@ -229,6 +230,7 @@ DELIMITER ;
 
 -- Series_ID
 DELIMITER $$
+
 CREATE TRIGGER	generate_Series_ID 
 before insert on Series
 For each ROW
@@ -262,6 +264,7 @@ DELIMITER ;
 
 -- User_ID
 DELIMITER $$
+
 CREATE TRIGGER generate_User_ID
 BEFORE INSERT On User
 For each row
@@ -275,8 +278,9 @@ begin
 END $$
 DELIMITER ;
 
-–notification
+--notification
 DELIMITER $$
+
 CREATE TRIGGER generate_welcome_notification
 AFTER INSERT On User
 For each row
@@ -434,7 +438,8 @@ DELIMITER ;
 
 -- add to accession number as new category is mapped to any book
  DELIMITER $$
- CREATE trigger update_Accession_on_category
+
+CREATE trigger update_Accession_on_category
  AFTER insert on Book_To_Category 
  FOR each row
  begin
@@ -447,7 +452,8 @@ DELIMITER ;
 
  -- Add into accession number as any entry occurs in Book to Author table
  DELIMITER $$
- CREATE trigger update_accession_on_author
+
+CREATE trigger update_accession_on_author
  AFTER insert on Book_To_Author
  FOR EACH ROW
  BEGIN
@@ -1055,14 +1061,14 @@ INSERT INTO Bookmarks (Bookmark_Note, Page_Number, Bookmark_Date, User_ID, Book_
 ('Foreshadowing of the climax.', 98, '2025-04-18', 'USE000000024', 'B00000000022'),
 ('Nice philosophical discussion.', 142, '2025-04-18', 'USE000000025', 'B00000000023'),
 ('Loved the setting described here.', 34, '2025-04-19', 'USE000000003', 'B00000000002'),
-('Character’s backstory explained.', 115, '2025-04-20', 'USE000000004', 'B00000000003'),
+('Character's backstory explained.', 115, '2025-04-20', 'USE000000004', 'B00000000003'),
 ('Plot hole? Need to think.', 90, '2025-04-20', 'USE000000006', 'B00000000005'),
 ('Insightful monologue.', 56, '2025-04-21', 'USE000000008', 'B00000000007'),
 ('Poetic ending.', 300, '2025-04-22', 'USE000000010', 'B00000000009');
 
 INSERT INTO Bookmarks (Bookmark_Note, Page_Number, Bookmark_Date, User_ID, Book_ID) VALUES
 ('First hint about the villain.', 45, '2025-04-12', 'USE000000013', 'B00000000011'),
-('Villain’s plan revealed.', 220, '2025-04-15', 'USE000000013', 'B00000000011'),
+('Villain's plan revealed.', 220, '2025-04-15', 'USE000000013', 'B00000000011'),
 
 ('Initial philosophy mentioned.', 30, '2025-04-13', 'USE000000014', 'B00000000012'),
 ('Deep discussion on morality.', 130, '2025-04-17', 'USE000000014', 'B00000000012'),
@@ -1091,7 +1097,7 @@ INSERT INTO Reviews (User_ID, Book_ID, Review_Text, Review_Date, Rating) VALUES
 ('USE000000016', 'B00000000014', 'Interesting concept but poor execution.', '2025-04-13', 3),
 ('USE000000017', 'B00000000015', 'Loved the depth of characters and world-building.', '2025-04-13', 5),
 ('USE000000018', 'B00000000016', 'Not my type of book, struggled to finish.', '2025-04-14', 2),
-('USE000000019', 'B00000000017', 'Couldn’t put it down! A thrilling masterpiece.', '2025-04-14', 5),
+('USE000000019', 'B00000000017', 'Couldn't put it down! A thrilling masterpiece.', '2025-04-14', 5),
 ('USE000000020', 'B00000000018', 'A decent book but not memorable.', '2025-04-15', 3),
 ('USE000000021', 'B00000000019', 'Incredible writing. Every page was a delight.', '2025-04-16', 5),
 ('USE000000022', 'B00000000020', 'Confusing at times, needed more clarity.', '2025-04-17', 2),
